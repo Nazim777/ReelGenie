fd06f51531c3083acc4894dd6e34f190e4ba85ac
Nazim777 <iamnazimhossain@gmail.com>
2025-12-16 10:04:50 +0600

    fix types error

--- src/app/api/verify-payment/route.ts
+++ src/app/api/verify-payment/route.ts
@@ -5,7 +5,7 @@ import { Users, Payments } from "@/config/schema";
 import { eq,sql } from "drizzle-orm";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2025-02-24.acacia",
 });
 
 export async function POST(req: Request) {
